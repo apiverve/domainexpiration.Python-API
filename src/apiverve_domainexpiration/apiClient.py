530216c4fcf85c14f9364a656e5f1a9d65bc525b
APIVerve Team <hello@apiverve.com>
2025-03-28 17:17:25 -0500

    Bug Fixes

--- src/apiverve_domainexpiration/apiClient.py
+++ src/apiverve_domainexpiration/apiClient.py
@@ -10,10 +10,10 @@ class DomainexpirationAPIClient:
         
         self.api_key = api_key
         self.secure = secure
-        self.base_url = 'https://api.apiverve.com/v1.0.9/domainexpiration'
+        self.base_url = 'https://api.apiverve.com/v1/domainexpiration'
         self.headers = {
             'x-api-key': self.api_key,
-            'auth-mode': 'pypi'
+            'auth-mode': 'pypi-package'
         }
 
     def execute(self, params=None):
